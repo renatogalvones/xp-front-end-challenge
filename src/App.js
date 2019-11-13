8209157fada7e99ad00781b707a2ff4efc9969ac
Renato Galvão <galvon@gmail.com>
2019-11-13 01:01:52 -0300

    feat(project): adds styledcomponents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Roboto", sans-serif;
+  }
+`;
+
+// import Routes from './Routes';
+import Main from './containers/Main';
 
 const App = () => (
-  <div>teste frontend</div>
+  <div>
+    <GlobalStyles />
+    <Route path="/" component={Main} />
+    {/* <Route path="/albums/:artist" component={Albums} /> */}
+  </div>
 )
 
 export default App;
---
+++ src/containers/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bar = styled.div`
+  background-color: #AAA;
+`;
+
+const Main = () => (
+  <Bar>HELLO PEOPLE</Bar>
+);
+
+export default Main;
